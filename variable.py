b1559e8cc0a29178a349d99785cd6e7a8d9d9f51
Oleg_Tikhomiroff <olega2111@yandex.ru>
2024-09-22 21:41:45 +0300

    Adding variable.py

---
+++ variable.py
@@ -0,0 +1,5 @@
+number_homework_completed=12
+number_hours_spent=1.5
+course_name='Python'
+time_one_task=number_hours_spent / number_homework_completed
+print('Курс: '+ course_name + str(', всего задач: ') + str(number_homework_completed) + str(', затрачено часов: ') + str(number_hours_spent) + str(', среднее время выполнения  ') + str(time_one_task) + str(' часа'))
