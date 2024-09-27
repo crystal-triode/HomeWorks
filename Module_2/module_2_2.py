e64b82980c09e2e07b36c8f920a9d48fa2654bfc
Oleg_Tikhomiroff <olega2111@yandex.ru>
2024-09-27 13:49:48 +0300

    27/09/24 - Added module_2_2.py

---
+++ Module_2/module_2_2.py
@@ -0,0 +1,9 @@
+first= int(float(input('Введите первое ЦЕЛОЕ число: ')))
+second = int(float(input('Введите второе ЦЕЛОЕ число: ')))
+third = int(float(input('Введите третье ЦЕЛОЕ число: ')))
+if first == second == third:
+    print("Одинаковых чисел - 3")
+elif first == second or first == third or second == third:
+    print('Одинаковых чисел - 2')
+else:
+    print('Одинаковых чисел -0')
