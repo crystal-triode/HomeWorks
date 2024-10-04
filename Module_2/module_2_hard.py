99d07247e10c0e1af5f6a39812e0160803a99d9c
Oleg_Tikhomiroff <olega2111@yandex.ru>
2024-10-04 12:30:57 +0300

    04/10/24 - module_2_hard.py fixed

--- Module_2/module_2_hard.py
+++ Module_2/module_2_hard.py
@@ -3,8 +3,10 @@ import random
 n = random.randint(3, 20)
 pairs = []
 
-for a in range(1, 11):
-    for b in range(1, 11):
+for a in range(1, n):
+    for b in range(1, n):
+        if b >= a:
+            continue
         s = a + b
         if n % s == 0:
             pairs.append((a, b))
